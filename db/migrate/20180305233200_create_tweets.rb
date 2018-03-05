2f7e987326628e2f55d61d9de7c2f367f05388f6
Joe OBrien <joe@objo.com>
2018-03-05 18:44:19 -0500

    adding in messages

---
+++ db/migrate/20180305233200_create_tweets.rb
@@ -0,0 +1,10 @@
+class CreateTweets < ActiveRecord::Migration[5.1]
+  def change
+    create_table :tweets do |t|
+      t.text :message
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180301000703) do
+ActiveRecord::Schema.define(version: 20180305233200) do
+
+  create_table "tweets", force: :cascade do |t|
+    t.text "message"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
