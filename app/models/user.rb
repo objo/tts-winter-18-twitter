382c7304071779c948370e0328a03d2738dd6c2f
Joe OBrien <joe@objo.com>
2018-03-12 18:59:05 -0400

    added fields for creating users and added states helper

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   
+  validates :username, uniqueness: true
+  
   has_many :tweets
   
   serialize :following, Array
