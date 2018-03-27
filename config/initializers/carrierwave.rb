af2496695969426fe232d98bfc2b6b925bdc6271
Joe OBrien <joe@objo.com>
2018-03-26 20:53:03 -0400

    getting rid of carrierwave varibles

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],        # required
-    aws_secret_access_key: ENV["AMAZON_SECRET_KEY"],     # required
+    aws_access_key_id:     "something",        # required
+    aws_secret_access_key: "something else",     # required
     region:                'us-east-1',                    # optional, defaults to 'us-east-1'
   }
   config.fog_directory  = 'tts-winter-objo' # required
